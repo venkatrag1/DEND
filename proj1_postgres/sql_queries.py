141348eb073f9f4a13dce2e01298fe85d4b74eb2
venkatrag1 <venkatraghavan.r@gmail.com>
2019-05-20 16:43:07 +0000

    songplays table added

--- proj1_postgres/sql_queries.py
+++ proj1_postgres/sql_queries.py
@@ -1,17 +1,32 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id INT, 
+    start_time TIMESTAMP, 
+    user_id INT, 
+    level int, 
+    song_id int, 
+    artist_id int, 
+    session_id int, 
+    location varchar, 
+    user_agent varchar,
+    PRIMARY KEY (songplay_id));
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users
+    (user_id INT, first_name VARCHAR, last_name VARCHAR,
+    gender VARCHAR,
+    PRIMARY KEY (user_id));    
 """)
 
 song_table_create = ("""
