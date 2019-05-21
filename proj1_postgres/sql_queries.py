92e23e14bff0e7532b85eb916709264da1fa648a
venkatrag1 <venkatraghavan.r@gmail.com>
2019-05-21 15:37:50 +0000

    finish all create

--- proj1_postgres/sql_queries.py
+++ proj1_postgres/sql_queries.py
@@ -10,32 +10,58 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id INT, 
+    songplay_id SERIAL PRIMARY KEY, 
     start_time TIMESTAMP, 
-    user_id INT, 
-    level int, 
-    song_id int, 
-    artist_id int, 
-    session_id int, 
-    location varchar, 
-    user_agent varchar,
-    PRIMARY KEY (songplay_id));
+    user_id INT NOT NULL, 
+    level VARCHAR, 
+    song_id VARCHAR NOT NULL, 
+    artist_id VARCHAR NOT NULL, 
+    session_id INT NOT NULL, 
+    location VARCHAR, 
+    user_agent VARCHAR
+    );
 """)
 
 user_table_create = ("""
-    CREATE TABLE IF NOT EXISTS users
-    (user_id INT, first_name VARCHAR, last_name VARCHAR,
+    CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY, 
+    first_name VARCHAR, 
+    last_name VARCHAR,
     gender VARCHAR,
-    PRIMARY KEY (user_id));    
+    level VARCHAR
+    );    
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+    song_id VARCHAR PRIMARY KEY,
+    title VARCHAR,
+    artist_id VARCHAR NOT NULL,
+    year SMALLINT, 
+    duration FLOAT
+    );
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR PRIMARY KEY,
+    name  VARCHAR,
+    location VARCHAR,
+    lattitude FLOAT,
+    longitude FLOAT
+    );
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+    start_time TIMESTAMP PRIMARY KEY,
+    hour SMALLINT, 
+    day SMALLINT,
+    week SMALLINT,
+    month SMALLINT,
+    year SMALLINT,
+    weekday VARCHAR
+    );
 """)
 
 # INSERT RECORDS
